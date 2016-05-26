ca92828e6e2bc12e2e096171620d0016673ac9cb
A. Maury <aurelien.maury+localgit@wescale.fr>
2016-05-27 01:09:46 +0200

    init

--- main.tf
+++ main.tf
@@ -1,27 +1,17 @@
-
-variable "bastion_subnet_id" {}
-variable "bastion_ami_id" {}
-variable "bastion_instance_type" {}
-variable "bastion_keypair" {}
-variable "bastion_security_group" {}
-
 resource "aws_instance" "bastion" {
-  ami = "${var.bastion_ami_id}"
-  instance_type = "${var.bastion_instance_type}"
-  key_name = "${var.bastion_keypair}"
-  subnet_id = "${var.bastion_subnet_id}"
-  security_groups = ["${var.bastion_security_group}"]
+
+  ami = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.keypair}"
+  subnet_id = "${var.subnet_id}"
+  security_groups = ["${split(',', var.security_groups}"]
 
   tags {
-    Name = "Bastion"
+    Name = "Bastion ${var.name}"
   }
 }
 
 resource "aws_eip" "bastion" {
   instance = "${aws_instance.bastion.id}"
   vpc = true
-}
-
-output "public_ip" {
-  value = "${aws_eip.bastion.public_ip}"
 }
